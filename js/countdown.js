4954378120ad4763eff98d9910ef897ac1dc9cb9
Damian Wiśniowski <dkontow@gmail.com>
2021-06-02 18:21:37 +0200

    add variables

--- js/countdown.js
+++ js/countdown.js
@@ -4,7 +4,6 @@ const spanMinutes = document.querySelector('.minutes');
 const spanSeconds = document.querySelector('.seconds');
 const date = document.querySelector('.dateValue');
 const time = document.querySelector('.timeValue');
-
 let endDate;
 if (localStorage.getItem('endDate')) {
     endDate = localStorage.getItem('endDate');
@@ -23,21 +22,25 @@ const getDate = () => {
         document.querySelector('.error').textContent = "Fields cannot be empty!";
     }
 }
+const addZero = (number) => {
+    number < 10 ? `0${number}` : number;
+}
 document.querySelector('button').addEventListener('click', getDate);
 const countdown = () => {
     const nowDate = new Date().getTime();
-    let days = Math.floor((endDate / (1000 * 60 * 60 * 24)) - (nowDate / (1000 * 60 * 60 * 24)));
+    const days = Math.floor((endDate / (1000 * 60 * 60 * 24)) - (nowDate / (1000 * 60 * 60 * 24)));
     let hours = Math.floor(((endDate / (1000 * 60 * 60)) - (nowDate / (1000 * 60 * 60))) % 24);
-    hours = hours < 10 ? `0${hours}` : hours;
+    let displayHours = hours < 10 ? `0${hours}` : hours;
     let minutes = Math.floor(((endDate / (1000 * 60)) - (nowDate / (1000 * 60))) % 60);
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    let displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
     let seconds = Math.floor(((endDate / (1000)) - (nowDate / (1000))) % 60);
-    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    let displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+    // seconds = seconds < 10 ? `0${seconds}` : seconds;
     if (endDate) {
         spanDays.innerHTML = `<p class='time'>${days} <span class='text'>days </span></p> `;
-        spanHours.innerHTML = `<p class='time'>${hours} <span class='text'>hours</span> </p> `;
-        spanMinutes.innerHTML = `<p class='time'>${minutes} <span class='text'>minutes</span> </p> `;
-        spanSeconds.innerHTML = `<p class='time'>${seconds} <span class='text'>seconds</span></p> `;
+        spanHours.innerHTML = `<p class='time'>${displayHours} <span class='text'>hours</span> </p> `;
+        spanMinutes.innerHTML = `<p class='time'>${displayMinutes} <span class='text'>minutes</span> </p> `;
+        spanSeconds.innerHTML = `<p class='time'>${displaySeconds} <span class='text'>seconds</span></p> `;
     }
 }
 countdown();
